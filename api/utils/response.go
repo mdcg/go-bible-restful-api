095137bb9e4e8a92d26e56eb94dc7fca8c017dfe
Mauro de Carvalho <mauro.dcg@gmail.com>
2020-08-04 01:13:29 -0300

    Adding docstrings to utils package

--- api/utils/response.go
+++ api/utils/response.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 )
 
+// Basic structure that meets the specifications of the JSEND format.
 type Payload struct {
 	status string
 	data   interface{}
 }
 
+// Function to facilitate the response of the API.
 func JSONResponse(w http.ResponseWriter, statusCode int, data interface{}) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(statusCode)
@@ -21,6 +23,7 @@ func JSONResponse(w http.ResponseWriter, statusCode int, data interface{}) {
 	w.Write([]byte(parsed_data))
 }
 
+// Function to format the data for successful responses.
 func SuccessResponse(payload interface{}) interface{} {
 	return map[string]interface{}{
 		"status": "success",
@@ -28,6 +31,7 @@ func SuccessResponse(payload interface{}) interface{} {
 	}
 }
 
+// Function to format the data for unsuccessful responses.
 func FailResponse(message string) map[string]interface{} {
 	return map[string]interface{}{
 		"status":  "fail",
